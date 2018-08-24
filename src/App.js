0d3cd1e4388bff0e7553b239c21682a2720e7c16
unknown <aymanmorsy.eg@gmail.com>
2018-08-24 10:10:30 +0200

    initial files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './map.css';
+
+
+
+class App extends Component {
+  componentDidMount(){
+
+  }
+
+ 
+
+  render() {
+    return (
+      <div className="App">
+        <p className='foo'>Hello world</p>
+        <div id="map"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
